768bc4fc96dc3beda5f6c251d056776c67591362
Douglas Fonseca <douglas.vasco79@gmail.com>
2022-09-10 23:05:14 -0300

    Alteração das configurações de banco

--- cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
+++ cambio-service/src/main/resources/db/migration/V1__Create_Table_Cambio.sql
@@ -3,4 +3,4 @@
   `from_currency` CHAR(3) NOT NULL,
   `to_currency` CHAR(3) NOT NULL,
   `conversion_factor` decimal(65,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4_unicode_ci;
